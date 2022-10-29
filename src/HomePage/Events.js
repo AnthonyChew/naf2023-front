9232e273f86f3d1bbe5a71c604b2680307809132
Samuel <schua045@e.ntu.edu.sg>
2022-10-29 17:09:12 +0800

    added logic to events, moved events svg to events folder

--- src/HomePage/Events.js
+++ src/HomePage/Events.js
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import LandingBg from './svgs/landingbg.svg';
-import EventLogo from './svgs/eventsLogo.png';
-import LeftArrow from './svgs/eventsLeftArrow.svg';
-import RightArrow from './svgs/eventsRightArrow.svg';
-import BigWhiteStar from './svgs/eventsBigWhiteStar.svg';
-import BigPurpleStar from './svgs/eventsBigPurpleStar.svg';
-import RedStar from './svgs/eventsRedStar.svg';
-import SmallPurpleStar from './svgs/eventsSmallPurpleStar.svg';
-import SmallWhiteStar from './svgs/eventsSmallWhiteStar.svg';
-import WhiteDot from './svgs/eventsWhiteDot.svg';
-import RedDot from './svgs/eventsRedDot.svg';
+import EventLogo from './svgs/events/eventsLogo.png';
+import LeftArrow from './svgs/events/eventsLeftArrow.svg';
+import RightArrow from './svgs/events/eventsRightArrow.svg';
+import BigWhiteStar from './svgs/events/eventsBigWhiteStar.svg';
+import BigPurpleStar from './svgs/events/eventsBigPurpleStar.svg';
+import RedStar from './svgs/events/eventsRedStar.svg';
+import SmallPurpleStar from './svgs/events/eventsSmallPurpleStar.svg';
+import SmallWhiteStar from './svgs/events/eventsSmallWhiteStar.svg';
+import WhiteDot from './svgs/events/eventsWhiteDot.svg';
+import RedDot from './svgs/events/eventsRedDot.svg';
+
+import Pikachu1 from './svgs/events/pikachu1.png';
+import Pikachu2 from './svgs/events/pikachu2.png';
+import Pikachu3 from './svgs/events/pikachu3.jpg';
+import Pikachu4 from './svgs/events/pikachu4.jpg';
 
 const Events = () => {
+  const imgGallery = [Pikachu1, Pikachu2, Pikachu3, Pikachu4];
+  const [imgState, setImgState] = useState(0)
+  const ImageState = (val) => (event) => {
+    if (val > 3) val = 0
+    if (val < 0) val = 3
+    setImgState(val)
+  }
+
   return (
     <div class="flex relative ">
-        <img src={RedDot} class="absolute top-[15%] left-[0%] z-10" ></img>
-        <img src={BigWhiteStar} class="absolute bottom-[10%] left-[-1%] z-10" ></img>
+      <img src={RedDot} class="absolute top-[15%] left-[0%] z-10" ></img>
+      <img src={BigWhiteStar} class="absolute bottom-[10%] left-[-1%] z-10" ></img>
 
 
       <div class="w-[70%] bg-NAFYellow grow relative">
         <img src={RedStar} class="absolute top-[-5%] right-[-8%] z-10" ></img>
         <img src={SmallPurpleStar} class="absolute bottom-[15%] right-[-2%]" ></img>
 
-        <div class="w-4/4 h-3/4 my-0 mx-auto p-10 relative">
-          
-        <img src={BigPurpleStar} class="absolute top-[-5%] left-[0%] z-0" ></img>
-          <img src="" class="h-full w-full"></img>
+        <div class="w-4/4 h-3/4 my-0 mx-auto p-10 relative max-w-[50%]">
+          <img src={BigPurpleStar} class="absolute top-[-5%] left-[0%] z-0" ></img>
+          <img src={imgGallery[imgState]} class="h-full w-full"></img>
         </div>
         <div class="mx-auto my-0 flex justify-around w-1/4">
-            <img class="inline-block" src={LeftArrow}></img>
-            <img class="inline-block" src={RightArrow}></img>
+          <img class="inline-block" src={LeftArrow} onClick={ImageState(imgState-1)}></img>
+          <img class="inline-block" src={RightArrow} onClick={ImageState(imgState+1)}></img>
         </div>
 
       </div>
@@ -40,22 +52,14 @@ const Events = () => {
         <div class="">
           <img src={EventLogo} class="w-3/4"></img>
           <div class="mt-15 w-4/5">
-            <div class="bg-white rounded-lg py-7 my-10 2xl:py-10">
-              <div class="font-yerkItalic text-4xl">EVENT NAME</div>
-              <div class="font-syne font-bold text-2xl">XX FEB 2023, 	&lt;Location	&gt;</div>
-            </div>
-            <div class="bg-white rounded-lg py-7 my-10 2xl:py-10">
-              <div class="font-yerkItalic text-4xl">EVENT NAME</div>
-              <div class="font-syne font-bold text-2xl">XX FEB 2023, 	&lt;Location	&gt;</div>
-            </div>
-            <div class="bg-white rounded-lg py-7 my-10 2xl:py-10">
-              <div class="font-yerkItalic text-4xl">EVENT NAME</div>
-              <div class="font-syne font-bold text-2xl">XX FEB 2023, 	&lt;Location	&gt;</div>
-            </div>
-            <div class="bg-white rounded-lg py-7 my-10 2xl:py-10">
-              <div class="font-yerkItalic text-4xl">EVENT NAME</div>
-              <div class="font-syne font-bold text-2xl">XX FEB 2023, 	&lt;Location	&gt;</div>
-            </div>
+            {
+              imgGallery.map((oneImage, index) => (
+                <div className={imgState == index ? "text-white bg-black rounded-lg py-7 my-10 2xl:py-10" : "text-black bg-white rounded-lg py-7 my-10 2xl:py-10"} onClick={ImageState(index)} >
+                  <div class="font-yerkItalic text-4xl">EVENT NAME</div>
+                  <div class="font-syne font-bold text-2xl">XX FEB 2023, 	&lt;Location	&gt;</div>
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
