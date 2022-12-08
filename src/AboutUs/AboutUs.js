5fecd1fe462d46efb8c254bca5c41bff4ab815ae
Jia Wei <kohjiawei1@gmail.com>
2022-12-08 23:40:43 +0800

    added the apple header stuff but not correct position

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -5,6 +5,7 @@ import FestGuide from './FestGuide'
 import Info from './Info'
 import WhatIsNTUArtsFestival from './svgs/WhatIsNTUArtsFestival.svg';
 import NTUArtsFestivalShadow from './svgs/NTUArtsFestivalShadow.svg';
+import AppleHeader from '../SharedPages/AppleHeader'
 
 /*
 import WhatIsNTUArtsFestival from './src/AboutUs/svgs';
@@ -35,8 +36,19 @@ const AboutUs = () => {
       <div class="min-h-screen bg-NAFYellow grow relative flex flex-col flex-wrap align-center">
         <img src={WhatIsNTUArtsFestival} class="absolute top-[5%] right-[45%] z-10 " ></img>
         <img src={NTUArtsFestivalShadow} class="absolute top-[6%] right-[45%] z-9 " ></img>
-        <h1 class="absolute top-[100%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
-
+        <h1 class="absolute top-[%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
+        <div class="w-[50%] h-[60%] right-20 scale-30 mb- mr-4 border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
+        <AppleHeader title={"www.aboutus.com"}></AppleHeader>
+        <div class="h-full w-full border-solid "  >
+          <div class="p-5 bg-white">
+            <iframe class="h-[35em] w-full" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          </div>
+          </div>
+            
+        <div class = "basis-1/2">
+          Right side stuff 
+        </div>
         </div>
  
         <FestGuide></FestGuide>
