3f599334637905bed9c63f10583e3a5bab2fb119
kiannylim <kianyew123@gmail.com>
2022-12-08 23:25:08 +0800

    added the stars

--- src/Marketplace/Cart.js
+++ src/Marketplace/Cart.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import CartBg from './svgs/Cart/bg.svg'
+import CartWord from './svgs/Cart/CART.png'
+
+import LeftTopYellowStar from './svgs/Cart/LeftTopYellowStar.svg'
+import LeftBlueStar from './svgs/Cart/LeftBlueStar.svg'
+import LeftBottomPurpleStar from './svgs/Cart/LeftBottomPurpleStar.svg'
+import RightTopPurpleStar from './svgs/Cart/RightTopPurpleStar.svg'
+import RightBlueStar from './svgs/Cart/RightBlueStar.svg'
+import BottomRightYellowStar from './svgs/Cart/BottomRightYellowStar.svg'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+
+    <div class="flex relative items-center min-h-screen bg-NAFPink bg-cover overflow-hidden" style={{ backgroundImage: `url(${CartBg})` }}>
+      
+      <img class="absolute top-[15%] left-[0%]" src={LeftTopYellowStar}></img>
+      <img class="absolute top-[50%] left-[10%]" src={LeftBlueStar}></img>
+      <img class="absolute top-[80%] left-[0%]" src={LeftBottomPurpleStar}></img>
+      <img class="absolute top-[8%] right-[0%]" src={RightTopPurpleStar}></img>
+      <img class="absolute top-[50%] right-[0%]" src={RightBlueStar}></img>
+      <img class="absolute top-[90%] right-[0%]" src={BottomRightYellowStar}></img>
+      
+      
+      <div class="absolute left-[0%] top-[0%] max-w-[30%] border-4 bg-white border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
+          <div class="flex items-center gap-[2em] flex-col pt-[1em] h-full w-full">
+            <img class="mt-11 mb-10 mx-43" src={CartWord}></img>
+          </div>
+      </div>
+
+      <div class="absolute right-[30%] absolute top - [20%] w-[50%] h-fit mb-28 mr-auto ml-36 border-4 border-black shadow-[20px_20px_0_0_rgba(255,255,0)]">
+        <div class="h-fit bg-white">
+        
+        </div>
+        
+      </div>
+
+      
+    </div>
   )
 }
 
