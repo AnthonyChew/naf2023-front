77a6283a96c3e3e7b729f59e73e2187494931447
kiannylim <kianyew123@gmail.com>
2022-12-09 00:29:00 +0800

    made cart symbol above the cart page

--- src/Marketplace/Cart.js
+++ src/Marketplace/Cart.js
@@ -13,29 +13,28 @@ const Cart = () => {
   return (
 
     <div class="flex relative items-center min-h-screen bg-NAFPink bg-cover overflow-hidden" style={{ backgroundImage: `url(${CartBg})` }}>
-      
-      <img class="absolute top-[15%] left-[0%]" src={LeftTopYellowStar}></img>
-      <img class="absolute top-[50%] left-[10%]" src={LeftBlueStar}></img>
-      <img class="absolute top-[80%] left-[0%]" src={LeftBottomPurpleStar}></img>
-      <img class="absolute top-[8%] right-[0%]" src={RightTopPurpleStar}></img>
-      <img class="absolute top-[50%] right-[0%]" src={RightBlueStar}></img>
-      <img class="absolute top-[90%] right-[0%]" src={BottomRightYellowStar}></img>
-      
-      
-      <div class="absolute left-[0%] top-[0%] max-w-[30%] border-4 bg-white border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
+      <div class = "absolute left-[12%] absolute top-[7.5%] w-[100%] ">
+        <div class="absolute left-[0%] top-[0%] z-[1] max-w-[100%] border-4 bg-white border-black shadow-[20px_20px_0_0_rgba(0,113,198)]">
           <div class="flex items-center gap-[2em] flex-col pt-[1em] h-full w-full">
             <img class="mt-11 mb-10 mx-43" src={CartWord}></img>
           </div>
-      </div>
-
-      <div class="absolute right-[30%] absolute top - [20%] w-[50%] h-fit mb-28 mr-auto ml-36 border-4 border-black shadow-[20px_20px_0_0_rgba(255,255,0)]">
-        <div class="h-fit bg-white">
-        
         </div>
-        
+
+        <div class="flex absolute top-[20%] z-[0] w-[50%] h-fit mt-[5%] mb-[14.5%] mr-[20%] ml-[6%] border-4 border-black shadow-[20px_20px_0_0_rgba(255,255,0)]">
+          <div class="h-fit bg-white pb-[80%]">
+              <p class="font-medium text-xl px-5 text-left">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget eros dui. Donec sit amet magna ligula. Mauris vitae diam aliquam, lobortis tellus sed, vulputate diam. Orci varius natoque penatibus et magnis dis parturient montes.
+              </p>
+          </div>
+        </div>
       </div>
 
-      
+      <img class="absolute top-[11.1%] left-[-0.0395%]" src={LeftTopYellowStar}></img>
+      <img class="absolute top-[50%] left-[9.45%]" src={LeftBlueStar}></img>
+      <img class="absolute top-[68%] left-[-1%]" src={LeftBottomPurpleStar}></img>
+      <img class="absolute top-[2%] right-[5%]" src={RightTopPurpleStar}></img>
+      <img class="absolute top-[26.25%] right-[0%]" src={RightBlueStar}></img>
+      <img class="absolute top-[90%] right-[0%]" src={BottomRightYellowStar}></img>
     </div>
   )
 }
