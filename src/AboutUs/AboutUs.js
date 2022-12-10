bab85db0d2563bcd6421f13e8e2dc889dfaa7408
Jia Wei <kohjiawei1@gmail.com>
2022-12-10 19:09:36 +0800

    Oh yeah, its all coming together
    https://www.youtube.com/watch?v=QyrDgEz3DR0

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -33,13 +33,15 @@ const AboutUs = () => {
     
     <div>
       <Navbar></Navbar>
-      <div class="min-h-screen bg-NAFYellow grow relative flex flex-col flex-wrap align-center">
-        <img src={WhatIsNTUArtsFestival} class="absolute top-[5%] right-[45%] z-10 " ></img>
-        <img src={NTUArtsFestivalShadow} class="absolute top-[6%] right-[45%] z-9 " ></img>
-        <h1 class="absolute top-[%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
-        <div class="w-[50%] h-[60%] right-20 scale-30 mb- mr-4 border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-        <AppleHeader title={"www.aboutus.com"}></AppleHeader>
-        <div class="h-full w-full border-solid "  >
+      <div class="min-h-screen bg-NAFYellow relative">
+        <img src={WhatIsNTUArtsFestival} class="absolute top-[5%] left-[5%] z-10 " ></img>
+        <img src={NTUArtsFestivalShadow} class="absolute top-[6%] left-[5%] z-9 " ></img>
+        <h1 class="max-w-[27%] flex-col absolute top-[30%] left-[5%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
+        <button class="font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ...">Download Fest Guide</button>
+        
+        <div class="relative right-[-30%] w-[30%] h-[60%] border-4 border-black shadow-[15px_20px_0_0_rgba(0,0,0)]">
+        <AppleHeader class="flex justify-center" title={"www.aboutus.com"}></AppleHeader>
+        <div class="h-full w-full border-solid"  >
           <div class="p-5 bg-white">
             <iframe class="h-[35em] w-full" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
