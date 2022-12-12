ad6aadfffd71bddfeb2dd60f55b356df0dc67caa
Samuel <schua045@e.ntu.edu.sg>
2022-12-13 00:30:47 +0800

    updated pagination to dot images for carousel

--- src/HomePage/Workshops.js
+++ src/HomePage/Workshops.js
@@ -29,7 +29,7 @@ const Workshops = () => {
       <div class="flex relative flex-col items-center">
         <img class="my-8 z-20" src={WorkshopLogo}></img>
       </div>
-      <div class="w-[80%] h-fit mb-28 mr-auto ml-36 border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)] bg-white">
+      <div class="w-[80%] h-fit mb-10 mr-auto ml-36 border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)] bg-white">
         <AppleHeader></AppleHeader>
         <div class="p-10">
 
@@ -42,6 +42,11 @@ const Workshops = () => {
           navigation={true}
           pagination={{
             el: '.swiper-custom-pagination',
+            clickable: true,
+            renderBullet: function(index, className) {
+              return '<span class="' + className + '"><img class="pagination-bullet"/></span>';
+          }
+  
           }}
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log('slide change')}
@@ -63,12 +68,13 @@ const Workshops = () => {
             <div class="text-center">Pikachu</div>
           </SwiperSlide>
         </Swiper>
-        <div class="text-center">
+
+        </div>
+
+      </div>
+      <div class="text-center mb-10">
           <div className="swiper-custom-pagination" />
         </div>
-        </div>
-      </div>
-
     </div>
   )
 }
