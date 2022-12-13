ffd54b449d0c71b217cbeb6c132b84f8ccaab040
Ian Sim <iansim396@gmail.com>
2022-12-13 20:02:52 +0800

    Improved Alignment + Added Responsiveness

--- src/AboutUs/Info.js
+++ src/AboutUs/Info.js
@@ -8,8 +8,8 @@ const Info = () => {
     <div class="bg-[#0071C6]">
 
       {/* SVGs */}
-      <img src={TopLeftPinkStar} class="absolute top-[3%] w-2/6 left-[-10%] z-30"></img>
-      <img src={MiddleBlueStar} class="absolute top-[6%] w-1/8 left-[50%] z-30"></img>
+      <img src={TopLeftPinkStar} class="absolute w-2/6 lg:w-1/4 top-10 md:top-2 lg:-top-10 -left-20 lg:-left-40 z-30"></img>
+      <img src={MiddleBlueStar} class="absolute w-1/6 lg:w-1/12 top-10 lg:top-5 left-[50%] z-30"></img>
 
       {/* <img src={Orbit} class="absolute top-[30%] w-4/6 right-[-13%] z-10"></img> */}
       {/* <img src={Orbit} class="scale-x-[-1.0] absolute top-[70%] w-4/6 left-[-13%] z-10"></img> */}
@@ -18,7 +18,7 @@ const Info = () => {
       {/* First Row */}
       <div class="flex flex-row pt-10">
         <div class="flex basis-1/2 text-sm justify-center items-center z-20">
-          <div class="mx-20 font-syne text-white">
+          <div class="mx-20 lg:mx-40 font-syne text-white text-base md:text-md lg:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia risus lorem, ut efficitur nisi facilisis id. Morbi molestie neque eu urna tincidunt lacinia. Mauris a massa sed orci vestibulum pretium. In hac habitasse platea dictumst. In hac habitasse platea dictumst. Etiam vitae lobortis lacus, at vestibulum mi. Mauris aliquet elit sed libero pharetra vestibulum.
           </div>
         </div>
@@ -35,7 +35,7 @@ const Info = () => {
           <div class="aspect-square box-border border-2 border-black bg-gray-300 w-4/6 shadow-[15px_10px_0_0_rgba(0,0,0)] z-20"></div>
         </div>
         <div class="flex basis-1/2 text-sm justify-center items-center z-20">
-          <div class="mx-20 font-syne text-white">
+          <div class="mx-20 lg:mx-40 font-syne text-white text-base md:text-md lg:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia risus lorem, ut efficitur nisi facilisis id. Morbi molestie neque eu urna tincidunt lacinia. Mauris a massa sed orci vestibulum pretium. In hac habitasse platea dictumst. In hac habitasse platea dictumst. Etiam vitae lobortis lacus, at vestibulum mi. Mauris aliquet elit sed libero pharetra vestibulum.
           </div>
         </div>
@@ -44,7 +44,7 @@ const Info = () => {
       {/* Third Row */}
       <div class="flex flex-row pb-10">
         <div class="flex basis-1/2 text-sm justify-center items-center z-20">
-          <div class="mx-20 font-syne text-white">
+          <div class="mx-20 lg:mx-40 font-syne text-white text-base md:text-md lg:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia risus lorem, ut efficitur nisi facilisis id. Morbi molestie neque eu urna tincidunt lacinia. Mauris a massa sed orci vestibulum pretium. In hac habitasse platea dictumst. In hac habitasse platea dictumst. Etiam vitae lobortis lacus, at vestibulum mi. Mauris aliquet elit sed libero pharetra vestibulum.
           </div>
         </div>
