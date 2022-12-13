3df3fbc73c14257997a7e0b77958c9a808e31236
Zhi <neozhixuan@gmail.com>
2022-12-14 00:49:03 +0800

    Added Sign Up Area
    Base changes

--- src/Workshop/SignUp.js
+++ src/Workshop/SignUp.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import LandingBg from "./svgs/workshop/landingbg.svg";
+import AppleHeader from "../SharedPages/AppleHeader";
+import SignUps from "./svgs/signups/workshops_signup.svg";
+import SignUpsShadow from "./svgs/signups/workshops_signupshadow.svg";
+import SignUpsStars from "./svgs/signups/workshops_signupstars.svg";
+import SignUpsButton from "./svgs/signups/workshops_signupbutton.svg";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div
+      class="h-auto md:min-h-screen w-full bg-NAFPurple py-20 px-[5%] lg:px-20"
+      style={{ backgroundImage: `url(${LandingBg})` }}
+    >
+      <div class="flex space-x-20">
+        {/* Top Left Apple Header */}
+        <div class="w-1/2 h-fit border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
+          <AppleHeader title={"www.signupguidelines.com"}></AppleHeader>
+          <p class="h-fit bg-white p-5 font-semibold text-center text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget
+            eros dui. Donec sit amet magna ligula. Mauris vitae diam aliquam,
+            lobortis tellus sed, vulputate diam. Orci varius natoque penatibus
+            et magnis dis parturient montes.
+          </p>
+        </div>
+        {/* Sign Up Logo */}
+        <div class="relative w-1/2">
+          <img class="z-20 absolute" src={SignUps} />
+          <img class="z-10 absolute top-2" src={SignUpsShadow} />
+          <img class="z-0 absolute -top-56 -left-16" src={SignUpsStars} />
+        </div>
+      </div>
+      <div class="relative bg-white ml-[12%] mt-10 w-10/12 h-fit border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
+        <AppleHeader title={"www.signupguidelines.com"}></AppleHeader>
+        {/* <p class="z-10 absolute top-[47%] left-[32%] text-4xl text-white">CLICK HERE TO SIGN UP</p>
+        <img class="mx-auto my-20" src={SignUpsButton} /> */}
+        <button class="flex my-20 py-[0.1%] px-[2.5%] text-[40px] text-white mx-auto" style={{ backgroundImage: `url(${SignUpsButton})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }} >CLICK HERE TO SIGN UP</button>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
--- src/Workshop/Workshops.js
+++ src/Workshop/Workshops.js
@@ -15,63 +15,66 @@ import workshops_ShadowBall from "./svgs/workshop/workshops_ShadowBall.svg";
 import workshops_StarBacking from "./svgs/workshop/workshops_StarBacking.svg";
 
 const classNames = {
-  tabActive: "text-xs md:text-base bg-white w-[32%] sm:w-[17%] pt-1 border-2 border-black font-semibold",
+  tabActive: " bg-white pt-1 border-2 border-black",
   tabInactive:
-    "text-xs md:text-base bg-gray-300 w-[32%] sm:w-[17%] border-y-2 border-y-gray-300 border-r-2 border-r-black py-1 font-semibold",
+    "bg-gray-300 border-y-2 border-y-gray-300 border-r-2 border-r-black py-1",
 };
 const Workshops = () => {
   const [active, setActive] = useState(0)
   return (
 
     <div
-      class="absolute h-auto md:min-h-screen w-full bg-NAFOrange py-20 px-[5%] lg:px-32"
-      style={{ backgroundImage: `url(${LandingBg})` }}
+      class="h-auto md:min-h-screen w-full bg-NAFOrange pb-20 px-[5%] lg:px-32"
     >
+      {/*  
       {/* Top Left */}
-      <img class="absolute left-1 top-7 w-[14%]" src={workshops_ShadowLeftStar}/>
-      <img class="absolute left-0 top-6 w-[14%]" src={workshops_BlueStar}/>
+      {/* <img class="absolute left-1 top-[30%] w-[14%]" src={workshops_ShadowLeftStar}/>
+      <img class="absolute left-0 top-[29%] w-[14%]" src={workshops_BlueStar}/> */}
       {/* Top Right */}
-      <img class="absolute right-1 top-[4%] w-[15%] sm:w-[20%]" src={workshops_ShadowRightStar}/>
-      <img class="absolute right-2 top-[3%] w-[15%] sm:w-[20%]" src={workshops_YellowRightStar}/>
+      {/* <img class="absolute right-1 top-[30%] w-[15%] sm:w-[20%]" src={workshops_ShadowRightStar}/>
+      <img class="absolute right-2 top-[29%] w-[15%] sm:w-[20%]" src={workshops_YellowRightStar}/> */}
       {/* Bottom Left */}
-      <img class="absolute left-1 bottom-3 w-[16%]" src={workshops_ShadowBottomLeftStar}/>
-      <img class="absolute left-0 bottom-4 w-[16%]" src={workshops_YellowBottomLeftStar}/>
+      {/* <img class="absolute left-1 bottom-3 w-[16%]" src={workshops_ShadowBottomLeftStar}/>
+      <img class="absolute left-0 bottom-4 w-[16%]" src={workshops_YellowBottomLeftStar}/> */}
       {/* Bottom Right */}
-      <img class="absolute right-0 bottom-4 w-[20%]" src={workshops_ShadowBottomRightStar}/>
-      <img class="absolute right-0 bottom-5 w-[20%]" src={workshops_PurpleBottomRightStar}/>
+      {/* <img class="absolute right-0 bottom-4 w-[20%]" src={workshops_ShadowBottomRightStar}/>
+      <img class="absolute right-0 bottom-5 w-[20%]" src={workshops_PurpleBottomRightStar}/> */}
       {/* Balls */}
-      <img class="absolute w-[1.5%] left-[15%] bottom-[3%] sm:left-[2%] sm:bottom-[30%] translate-x-0.5 translate-y-0.5" src={workshops_ShadowBall}/>
+      {/* <img class="absolute w-[1.5%] left-[15%] bottom-[3%] sm:left-[2%] sm:bottom-[30%] translate-x-0.5 translate-y-0.5" src={workshops_ShadowBall}/>
       <img class="absolute w-[1.5%] left-[15%] bottom-[3%] sm:left-[2%] sm:bottom-[30%]" src={workshops_WhiteBall}/>
       <img class="absolute w-[1.5%] right-[18%] sm:right-[25%] bottom-[3%] translate-x-0.5 translate-y-0.5" src={workshops_ShadowBall}/>
       <img class="absolute w-[1.5%] right-[18%] sm:right-[25%] bottom-[3%]" src={workshops_WhiteBall}/>
       <img class="absolute w-[1.5%] right-[16%] sm:right-[3%] top-[12%] sm:top-[33%] translate-x-0.5 translate-y-0.5" src={workshops_ShadowBall}/>
-      <img class="absolute w-[1.5%] right-[16%] sm:right-[3%] top-[12%] sm:top-[33%]" src={workshops_WhiteBall}/>
-      <img class="absolute md:-top-[15px] top-[5px] right-[27%] w-[45%]" src={workshops_StarBacking}/>
-      <img class="absolute top-[20px] right-[20%] mt-[6%] w-[58%]" src={workshops_Logo}/>
+      <img class="absolute w-[1.5%] right-[16%] sm:right-[3%] top-[12%] sm:top-[33%]" src={workshops_WhiteBall}/> */}
+      
       {/* Header */}
-      {/* <div
-        class="h-20 mb-20"
-        style={{
-          backgroundImage: `url(${workshops_Logo})`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          zIndex: "1000"
-        }}
-      /> */}
-
-      {/* Workshops Body */}
-      <div class="flex w-full mt-5 sm:mt-[15%]">
-        <div onClick={() => setActive(0)} class={`${active === 0 ? classNames.tabActive : classNames.tabInactive}`}>Henna Tattoos</div>
-        <div onClick={() => setActive(1)} class={`${active === 1 ? classNames.tabActive : classNames.tabInactive}`}>Photobooth</div>
-        <div onClick={() => setActive(2)} class={`${active === 2 ? classNames.tabActive : classNames.tabInactive}`}>Art Movie Screening</div>
+      <img class="relative pt-10 pb-5 ml-[23%] w-[58%]" src={workshops_Logo}/>
+      <div class="z-10 relative mb-10 mx-[10%] lg:mx-[15%] font-semibold text-center text-xs sm:text-sm md:text-md lg:text-xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+      mollit anim id est laborum.
       </div>
-      <div class="flex border-solid bg-white border-2 border-black w-full shadow-[20px_20px_0_0_rgba(0,0,0)] p-[5%]">
+      {/* Stars */}
+      <img class="z-0 absolute top-[40%] right-[3%] w-screen" src={workshops_StarBacking}/>
+      {/* Workshops Body */}
+      <div class="z-10 relative flex w-full mt-5 ">
+        <div onClick={() => setActive(0)} class={`${active === 0 ? classNames.tabActive : classNames.tabInactive} text-xs md:text-base w-[32%] sm:w-[17%] font-semibold text-center`}>Henna Tattoos</div>
+        <div onClick={() => setActive(1)} class={`${active === 1 ? classNames.tabActive : classNames.tabInactive} text-xs md:text-base w-[32%] sm:w-[17%] font-semibold text-center`}>Photobooth</div>
+        <div onClick={() => setActive(2)} class={`${active === 2 ? classNames.tabActive : classNames.tabInactive} text-xs md:text-base w-[32%] sm:w-[17%] font-semibold text-center`}>Art Movie Screening</div>
+      </div>
+      
+      {/*  */}
+      <div class="z-10 relative flex border-solid bg-white border-2 border-black w-full shadow-[20px_20px_0_0_rgba(0,0,0)] p-[5%]">
         {/* Image */}
-        <div class="bg-gray-300 h-auto lg:h-80 w-4/12 mr-[8%] flex items-center justify-center">
+        <div class=" bg-gray-300 h-auto lg:h-80 w-4/12 mr-[8%] flex items-center justify-center">
           IMAGE OF WORKSHOP
         </div>
         {/* Text Area */}
-        <div class="flex w-8/12 flex-col gap-y-2 sm:gap-y-5">
+        <div class=" flex w-8/12 flex-col gap-y-2 sm:gap-y-5">
           <p class="font-Ubuntu sm:text-lg md:text-2xl lg:text-4xl font-bold flex justify-self-start">{active === 0 ? "Henna Tattoos" : active === 1 ? "Photobooth" : "Art Movie Screening"}</p>
           <p class="text-left text-[10px] sm:text-sm md:text-md lg:text-lg font-semibold">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
