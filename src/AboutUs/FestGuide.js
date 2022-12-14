c88557eb58e00d41251eba689db3c3d789d86dbc
Jia Wei <kohjiawei1@gmail.com>
2022-12-14 19:29:24 +0800

    Good on PC, not on mobile
    Things to work on
    
    Button
    
    Phone size

--- src/AboutUs/FestGuide.js
+++ src/AboutUs/FestGuide.js
@@ -1,18 +1,18 @@
 import React from "react";
-import WhatIsNTUArtsFestival from "./svgs/WhatIsNTUArtsFestival.png";
+import WhatIsNTUArtsFestival from "./svgs/FestGuide/WhatIsNTUArtsFestival.png";
 import AppleHeader from "../SharedPages/AppleHeader";
-import BigWhiteStar from "./svgs/BigWhiteStar.svg";
-import MediumRedStar from "./svgs/MediumRedStar.svg";
-import MediumYellowStar from "./svgs/MediumYellowStar.svg";
-import SmallBlueStar from "./svgs/SmallBlueStar.svg";
-import SmallPurpleDot from "./svgs/SmallPurpleDot.svg";
-import SmallRedStar from "./svgs/SmallRedStar.svg";
-import SmallWhiteDot from "./svgs/SmallWhiteDot.svg";
-import MediumPurpleStar from "./svgs/MediumPurpleStar.svg";
+import BigWhiteStar from "./svgs/FestGuide/BigWhiteStar.svg";
+import MediumRedStar from "./svgs/FestGuide/MediumRedStar.svg";
+import MediumYellowStar from "./svgs/FestGuide/MediumYellowStar.svg";
+import SmallBlueStar from "./svgs/FestGuide/SmallBlueStar.svg";
+import SmallPurpleDot from "./svgs/FestGuide/SmallPurpleDot.svg";
+import SmallRedStar from "./svgs/FestGuide/SmallRedStar.svg";
+import SmallWhiteDot from "./svgs/FestGuide/SmallWhiteDot.svg";
+import MediumPurpleStar from "./svgs/FestGuide/MediumPurpleStar.svg";
 
 const FestGuide = () => {
   return (
-    <div class="overflow-hidden flex relative min-h-screen bg-NAFYellow">
+    <div class="overflow-y-clip flex relative min-h-screen bg-NAFYellow">
       {/* <div>
           <h1 class="basis-1/2 z-10 max-w-[27%] flex-col absolute top-[30%] left-[5%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
           <img src={WhatIsNTUArtsFestival} class="absolute top-[5%] left-[5%] z-20 " ></img>
@@ -48,7 +48,7 @@ const FestGuide = () => {
 
         <div class="relative basis-1/2">
           <img src={WhatIsNTUArtsFestival} class=""></img>
-          <h1 class="my-10 font-syne font-normal text-2xl ">
+          <h1 class="my-5 mr-10 font-syne font-normal text-2xl ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -59,7 +59,7 @@ const FestGuide = () => {
           </h1>
           <button
             onclick="getFestGuide()"
-            class="hover:bg-violet-700 h-[7%] w-[45%] items-center justify-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ..."
+            class="hover:bg-violet-700 mt-2 h-[7%] w-[45%] items-center justify-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ..."
           >
             DOWNLOAD FEST GUIDE
           </button>
@@ -68,45 +68,18 @@ const FestGuide = () => {
           <div class="mx-auto w-[80%] h-[80%] bg-white align-center border-4 border-black shadow-[15px_20px_0_0_rgba(0,0,0)]">
             <AppleHeader></AppleHeader>
             <div class="h-[100%] border-solid p-5">
-              
-                
-                  <iframe
-                    class=" w-full h-[95%]" 
-                    src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-                    title="Rick Astley - Never Gonna Give You Up (Official Music Video)"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
-                  ></iframe>
-                </div>
-              
-            
-          </div>
-        </div>
-
-        {/*<button class="flex flex-1 h-5 w-2 items-center justify-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ...">Download Fest Guide</button>*/}
-      </div>
-
-      {/* <div class="flex flex-1 items-center justify-center ">
-        <h1 class="relative p-5 font-syne font-normal basis-1/1 z-10 text-2xl max-w-[67%] flex-col left-[-2.5%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
-
-        <button onclick="getFestGuide()" class="hover:bg-violet-700 relative p-3 top-[70%] left-[7.8%] font-syne font-bold text-zinc-50 px-1 border-8 border-indigo-500/10 bg-NAFPurple rounded-lg ...">DOWNLOAD FEST GUIDE</button>
-        </div>
-
-        
-        <div class="flex flex-1 items-center justify-center z-20">
-          <div class=" w-[80%] border-4 border-black shadow-[15px_20px_0_0_rgba(0,0,0)]">
-
-            <AppleHeader></AppleHeader>
-            <div class="h-full w-full border-solid">
-              <div class="h-fit bg-white ">
-                <div class="flex items-center p-5">
-                  <iframe class="h-[50em] w-full" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-              </div>
+              <iframe
+                class=" w-full h-[95%]"
+                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                title="Rick Astley - Never Gonna Give You Up (Official Music Video)"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </div>
           </div>
-        </div>  */}
+        </div>
+      </div>
     </div>
   );
 };
