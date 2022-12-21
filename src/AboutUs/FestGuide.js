c68e7c1bbcd490db0527c9a7f5ca8e34651e1746
Jia Wei <kohjiawei1@gmail.com>
2022-12-21 11:10:44 +0800

    fixed button

--- src/AboutUs/FestGuide.js
+++ src/AboutUs/FestGuide.js
@@ -58,7 +58,7 @@ const FestGuide = () => {
           </h1>
 {/*./pdfs/Sample pdf document.pdf  padding: 15px 32px; */}
           <a type="submit" href="/AboutUs/pdfs/Sample pdf document.pdf" download="Sample pdf document.pdf"
-            class="hover:bg-violet-700 mt-2 h-[7%] w-[45%] p-15  box-border text-center items-center justify-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ..."
+            class="hover:bg-violet-700 py-[5%] mt-2 w-[45%] p-15  box-border text-center items-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ..."
           >
             DOWNLOAD FEST GUIDE
           </a>
--- src/AboutUs/Nebula.js
+++ src/AboutUs/Nebula.js
@@ -6,13 +6,11 @@ const Nebula = () => {
   return (
     <div class="overflow-y-clip flex relative min-h-screen bg-NAFPurple ">
         <div class="flex relative flex-col items-center">
-          <img class="mt-28 mb-28 z-20" src={NebulaImg}></img>
-      <div class = "flex relative flex-col items-center">
-        <img src={WhatsOnLogo} class = "mt-28 mb-28 z-20 "></img>
-        </div>
+          <img class="mx-14 mt-28 z-20" src={NebulaImg}></img>
         </div>
     </div>
   );
+  
 };
 
 export default Nebula;
