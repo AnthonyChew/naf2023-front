c709a31b1c63dd208832655bfd1673352e43e41a
Jia Wei <kohjiawei1@gmail.com>
2022-12-21 11:23:11 +0800

    Nebula.js done

--- src/AboutUs/Nebula.js
+++ src/AboutUs/Nebula.js
@@ -8,6 +8,7 @@ const Nebula = () => {
       <div class="relative basis-1/2"></div>
       <div class="flex relative flex-col items-center">
           <img class="mx-14 mt-28 flex z-20 items-center" src={NebulaImg}></img>
+          <div class="text-white font-syne font-normal text-xl pt-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
         </div>
       <div class="relative basis-1/2"></div>
         
