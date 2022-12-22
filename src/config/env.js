a4241b8deb266cc7fdb4698421ab93b28c463216
anthonychew <anthony.chew@hotmail.com>
2022-12-22 13:50:42 +0800

    Update env

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@
 // Good practice for passing environment variables into the code
 // Instead of calling process.env everywhere, import from this file
 const envConfig = {
-  backendUrl: process.env.REACT_APP_BACKEND,
+  backendUrl: process.env.REACT_APP_BACKEND_URL,
   endpoints: {
     workshops: 'workshops',
     products: 'products',
