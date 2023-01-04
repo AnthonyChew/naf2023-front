9359860a9dae66735c68b1115b3276931f423bc5
Zhi <neozhixuan@gmail.com>
2023-01-04 21:02:54 +0800

    Completed page (last area not responsive)

--- src/HomePage/Workshops.js
+++ src/HomePage/Workshops.js
@@ -1,34 +1,52 @@
-import React from 'react'
-import { Navigation, Pagination, Scrollbar, A11y, EffectCube, EffectFade } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCube,
+  EffectFade,
+} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/effect-cube';
-import 'swiper/css/effect-fade';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/effect-cube";
+import "swiper/css/effect-fade";
 
-
-
-import WorkshopLogo from './svgs/workshop/WorkshopLogo.png'
-import AppleHeader from '../SharedPages/AppleHeader';
-import WorkshopTopLeftPurpleStar from './svgs/workshop/WorkshopPurpleStar.svg'
-import WorkshopTopRightRedDot from './svgs/workshop/WorkshopRedDot.svg'
-import WorkshopTopRightYellowStar from './svgs/workshop/WorkshopYellowStar.svg'
-import WorkshopBottomRightBlueStar from './svgs/workshop/WorkshopBlueStar.svg'
-import LandingBg from './svgs/landing/landingbg.svg'
+import WorkshopLogo from "./svgs/workshop/WorkshopLogo.png";
+import AppleHeader from "../SharedPages/AppleHeader";
+import WorkshopTopLeftPurpleStar from "./svgs/workshop/WorkshopPurpleStar.svg";
+import WorkshopTopRightRedDot from "./svgs/workshop/WorkshopRedDot.svg";
+import WorkshopTopRightYellowStar from "./svgs/workshop/WorkshopYellowStar.svg";
+import WorkshopBottomRightBlueStar from "./svgs/workshop/WorkshopBlueStar.svg";
+import LandingBg from "./svgs/landing/landingbg.svg";
 
 const Workshops = () => {
   return (
-    <div class="bg-NAFOrange bg-cover min-h-screen relative overflow-hidden" style={{ backgroundImage: `url(${LandingBg})`}}>
-        <img src={WorkshopTopLeftPurpleStar} class="absolute top-[5%] left-[0%] z-10"></img>
-        <img src={WorkshopTopRightRedDot} class="absolute top-[2%] right-[10%] z-10"></img>
-        <img src={WorkshopTopRightYellowStar} class="absolute top-[5%] right-[0%] z-10"></img>
-        <img src={WorkshopBottomRightBlueStar} class="absolute bottom-[-8%] right-[-5%] z-10"></img>
-
-
+    <div
+      class="bg-NAFOrange bg-cover min-h-screen relative overflow-hidden"
+      style={{ backgroundImage: `url(${LandingBg})` }}
+    >
+      <img
+        src={WorkshopTopLeftPurpleStar}
+        class="absolute top-[5%] left-[0%] z-10"
+      ></img>
+      <img
+        src={WorkshopTopRightRedDot}
+        class="absolute top-[2%] right-[10%] z-10"
+      ></img>
+      <img
+        src={WorkshopTopRightYellowStar}
+        class="absolute top-[5%] right-[0%] z-10"
+      ></img>
+      <img
+        src={WorkshopBottomRightBlueStar}
+        class="absolute bottom-[-8%] right-[-5%] z-10"
+      ></img>
 
       <div class="flex relative flex-col items-center">
         <img class="my-8 z-20" src={WorkshopLogo}></img>
@@ -36,52 +54,70 @@ const Workshops = () => {
       <div class="w-[80%] h-fit mb-10 mr-auto ml-36 border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)] bg-white">
         <AppleHeader></AppleHeader>
         <div class="p-10">
-
-        
-        <Swiper
-          // install Swiper modules
-          modules={[Navigation, Pagination, Scrollbar, A11y, EffectCube, EffectFade]}
-          spaceBetween={50}
-          slidesPerView={3}
-          navigation={true}
-          pagination={{
-            el: '.swiper-custom-pagination',
-            clickable: true,
-            renderBullet: function(index, className) {
-              return '<span class="' + className + '"><img class="pagination-bullet"/></span>';
-          }
-  
-          }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log('slide change')}
-          loop={true}
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
-            <div class="text-center">Pikachu</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
-            <div class="text-center">Pikachu</div>
-          </SwiperSlide>
-           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
-            <div class="text-center">Pikachu</div>
-          </SwiperSlide>
-           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
-            <div class="text-center">Pikachu</div>
-          </SwiperSlide>
-        </Swiper>
-
+          <Swiper
+            // install Swiper modules
+            modules={[
+              Navigation,
+              Pagination,
+              Scrollbar,
+              A11y,
+              EffectCube,
+              EffectFade,
+            ]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation={true}
+            pagination={{
+              el: ".swiper-custom-pagination",
+              clickable: true,
+              renderBullet: function (index, className) {
+                return (
+                  '<span class="' +
+                  className +
+                  '"><img class="pagination-bullet"/></span>'
+                );
+              },
+            }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            loop={true}
+          >
+            <SwiperSlide>
+              <img
+                src="https://swiperjs.com/demos/images/nature-1.jpg"
+                alt=""
+              />
+              <div class="text-center">Pikachu</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://swiperjs.com/demos/images/nature-2.jpg"
+                alt=""
+              />
+              <div class="text-center">Pikachu</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://swiperjs.com/demos/images/nature-3.jpg"
+                alt=""
+              />
+              <div class="text-center">Pikachu</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://swiperjs.com/demos/images/nature-4.jpg"
+                alt=""
+              />
+              <div class="text-center">Pikachu</div>
+            </SwiperSlide>
+          </Swiper>
         </div>
-
       </div>
       <div class="text-center mb-10">
-          <div className="swiper-custom-pagination" />
-        </div>
+        <div className="swiper-custom-pagination" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Workshops
+export default Workshops;
--- src/Workshop/Gallery.js
+++ src/Workshop/Gallery.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Logo from "./svgs/gallery/workshops_gallerylogo.svg";
 import LogoShadow from "./svgs/gallery/workshops_gallerylogoshadow.svg";
+import File from "./svgs/gallery/workshops_file.svg";
+import Planet1 from "./svgs/gallery/workshops_planet1.svg";
+import Planet2 from "./svgs/gallery/workshops_planet2.svg";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Grid, Pagination } from "swiper";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCube,
+  EffectFade,
+  Grid,
+} from "swiper";
+
+import "swiper/css";
+import "swiper/css/grid"; // need this
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/effect-cube";
+import "swiper/css/effect-fade";
+import "./styles.css";
 
 const Gallery = () => {
+  const pagination = {
+    el: ".swiper-custom-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<span class="' +
+        className +
+        '"><img class="pagination-bullet"/></span>'
+      );
+    },
+  };
+  const navigation = {
+    nextEl: ".gallery-nextEl",
+    prevEl: ".gallery-prevEl",
+  };
+
   return (
     <div class="overflow-hidden relative h-auto md:min-h-screen w-full bg-NAFBlue pt-12 md:pb-0 pb-20 px-[5%] lg:px-20">
+      <img className="z-0 absolute -bottom-[5%] left-0 w-[47%]" src={Planet1} />
+      <img
+        className="z-0 absolute -bottom-[23%] right-0 w-[47%] "
+        src={Planet2}
+      />
       <img class="absolute mt-1 ml-[23%] w-[45%]" src={LogoShadow} />
       <img class="absolute ml-[23%] mb-10 w-[45%]" src={Logo} />
       <div class="z-10 relative mt-[6%] mb-10 mx-[10%] lg:mx-[15%] font-semibold text-white text-center text-xs sm:text-sm md:text-md lg:text-xl">
@@ -18,46 +59,54 @@ const Gallery = () => {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </div>
-      <Swiper
-        className="mySwiper mx-auto w-[80%] !h-[100vh] !mx-[10%] !lg:mx-[15%]"
-        slidesPerView={3}
-        slidesPerColumn={2}
-        grid={{
-          rows: 2,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Grid, Pagination]}
-      >
-        <SwiperSlide className="bg-white !h-[40%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 1
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[40%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 2
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[40%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 3
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[40%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 4
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[30%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 5
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[30%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 6
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[30%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 7
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[30%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 8
-        </SwiperSlide>
-        <SwiperSlide className="bg-white ml-[6.333%] !h-[30%] !w-[27%] border-solid border-4 border-black shadow-[20px_20px_0_0_rgba(0,0,0)]">
-          Slide 9
-        </SwiperSlide>
-      </Swiper>
+      <div className="flex flex-row">
+        <div className="z-10 gallery-prevEl" />
+        <div className="h-[800px] mb-5 flex flex-row !w-[80%]">
+          <Swiper
+            // install Swiper modules
+            className="gallery-swiper"
+            modules={[
+              Navigation,
+              Pagination,
+              Scrollbar,
+              A11y,
+              EffectCube,
+              EffectFade,
+              Grid,
+            ]}
+            spaceBetween={50}
+            slidesPerView={3}
+            grid={{
+              rows: 2,
+            }}
+            navigation={navigation}
+            pagination={pagination}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide className="gallery-swiper-slide">Slide 1</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 2</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 3</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 4</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 5</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 6</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 7</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 8</SwiperSlide>
+            <SwiperSlide className="gallery-swiper-slide">Slide 9</SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="z-10 gallery-nextEl" />
+      </div>
+      <div className="text-center mb-5">
+        <div className="swiper-custom-pagination" />
+      </div>
+      <div className="flex flex-row justify-center justify-between mx-[20%] mb-[20%]">
+        <img className="z-10 w-[10%]" src={File} />
+        <img className="z-10 w-[10%]" src={File} />
+        <img className="z-10 w-[10%]" src={File} />
+        <img className="z-10 w-[10%]" src={File} />
+        <img className="z-10 w-[10%]" src={File} />
+      </div>
     </div>
   );
 };
