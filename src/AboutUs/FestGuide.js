b8b698b471c535704d0672051f73dfb09b9005b4
Samuel <schua045@e.ntu.edu.sg>
2023-01-06 09:33:53 +0800

    update responsiveness for festguide

--- src/AboutUs/FestGuide.js
+++ src/AboutUs/FestGuide.js
@@ -11,9 +11,9 @@ import SmallWhiteDot from "./svgs/FestGuide/SmallWhiteDot.svg";
 import MediumPurpleStar from "./svgs/FestGuide/MediumPurpleStar.svg";
 
 const FestGuide = () => {
-  
+
   return (
-    <div class="overflow-y-clip flex relative min-h-screen bg-NAFYellow">
+    <div class="flex relative lg:min-h-screen bg-NAFYellow">
       {/* <div>
           <h1 class="basis-1/2 z-10 max-w-[27%] flex-col absolute top-[30%] left-[5%]" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h1>
           <img src={WhatIsNTUArtsFestival} class="absolute top-[5%] left-[5%] z-20 " ></img>
@@ -32,7 +32,7 @@ const FestGuide = () => {
           </div>
         </div>  */}
 
-      <div class=" flex flex-row w-[100%] min-h-screen mt-32 mx-10 my-10 flex-wrap">
+      <div class=" flex flex-row w-[100%] mt-20 lg:mt-32 mx-10 sm:mb-20 md:mb-0 flex-wrap">
         <img src={BigWhiteStar} class="absolute top-[40%] right-[0%] hidden lg:block"></img>
         {/* <img src={MediumRedStar} class="absolute top-[87%] left-[0%] "></img> */}
         <img src={MediumYellowStar} class="absolute top-[-8%] right-[9%]"></img>
@@ -45,9 +45,9 @@ const FestGuide = () => {
         <img src={SmallRedStar} class="absolute top-[9%] right-[4%] z-10 hidden lg:block"></img>
         <img src={SmallWhiteDot} class="absolute top-[90%] left-[12%] hidden lg:block"></img>
 
-        <div class="relative lg:basis-1/2 max-md:w-fit max-md:basis-auto ">
+        <div class="relative lg:basis-1/2 mb-10 lg:mb-0 ">
           <img src={WhatIsNTUArtsFestival} class=""></img>
-          <h1 class="my-5 mr-10 font-syne font-normal text-2xl ">
+          <h1 class="my-5 md:mr-10 font-syne font-normal text-lg lg:text-2xl ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -56,16 +56,16 @@ const FestGuide = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum."
           </h1>
-{/*./pdfs/Sample pdf document.pdf  padding: 15px 32px; */}
+          {/*./pdfs/Sample pdf document.pdf  padding: 15px 32px; */}
           <a type="submit" href="/AboutUs/pdfs/Sample pdf document.pdf" download="Sample pdf document.pdf"
-            class="max-md:w-auto max-md:flex mb-[2%] justify-center hover:bg-violet-700 py-[5%] mt-2 w-[45%] p-15 box-border text-center items-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg ..."
+            class="justify-center hover:bg-violet-700 py-[5%] mt-2 w-[300px] h-[60px] py-[17px] box-border text-center items-center font-syne font-weight: 700; text-zinc-50 px-2 border-2 border-indigo-500/100 bg-NAFPurple rounded-lg"
           >
             DOWNLOAD FEST GUIDE
           </a>
-          
+
         </div>
-        <div class="lg:relative lg:basis-1/2 max-md:place-self-center">
-          <div class="max-md:w-fit  mx-auto w-[80%] h-[80%] bg-white align-center border-4 border-black shadow-[15px_20px_0_0_rgba(0,0,0)]">
+        <div class="lg:relative lg:basis-1/2 basis-full mb-20">
+          <div class="mx-auto w-[100%] lg:w-[80%] h-[100%] bg-white align-center border-4 border-black shadow-[15px_20px_0_0_rgba(0,0,0)]">
             <AppleHeader></AppleHeader>
             <div class="h-[100%] border-solid p-5">
               <iframe
