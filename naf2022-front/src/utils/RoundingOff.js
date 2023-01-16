08e939542dad2396a00d2a252db459fc1ac944b3
anthonychew <anthony.chew@hotmail.com>
2023-01-16 17:22:18 +0800

    Update remove naf2022

--- naf2022-front/src/utils/RoundingOff.js
+++
@@ -1,6 +0,0 @@
-export default function roundTo(n) {
-  const multiplicator = Math.pow(10, 2);
-  n = parseFloat((n * multiplicator).toFixed(11));
-  const test = Math.round(n) / multiplicator;
-  return +test.toFixed(2);
-}
