a9396a31a3d0a17c3df059340d1448168d384af2
anthonychew <anthony.chew@hotmail.com>
2023-01-25 13:39:55 +0800

    Update payment not workinggggg

--- src/Marketplace/Payment.js
+++ src/Marketplace/Payment.js
@@ -185,20 +185,31 @@ const Payment = () => {
     delete data['deliveryAddress'];
     data.purchases = purchases;
     data.total = parseFloat(totalPrice);
-    data = { ...data, newImages: images[0], images: [] };
+
+    const form_data = new FormData();
+
+    for (var key in data) {
+      if(key === 'contactNumber')
+      {
+      form_data.append(key, data[key]);
+      }
+      form_data.append(key, JSON.stringify(data[key]));
+    }
+    form_data.append('newImages', images[0]);
+    form_data.append('images', JSON.stringify([]));
 
     //console.log(form_data.get('newImages'));
-    const res = await trackPromise(orderService.postOrder(data));
-    if (res.status === 200) {
-      dispatch(resetCart());
-      history('/submitted');
-    } else if (res.status === 401) {
-      // console.log(res);
-      setAuth(false);
-      alert('Please login to complete your payment');
-    } else {
-      alert(res.data.error);
-    }
+    // const res = await trackPromise(orderService.postOrder(form_data));
+    // if (res.status === 200) {
+    //   dispatch(resetCart());
+    //   history('/submitted');
+    // } else if (res.status === 401) {
+    //   // console.log(res);
+    //   setAuth(false);
+    //   alert('Please login to complete your payment');
+    // } else {
+    //   alert(res.data.error);
+    // }
   }
 
   const schema = yup.object().shape({
