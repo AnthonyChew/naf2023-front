f65f120cd04d20613858a6b5a314a1cfbb3547d2
Shan <44370959+Shanzzzzz@users.noreply.github.com>
2023-01-28 02:39:52 +0800

    small fixes for responsiveness

--- src/Partners/Partners.js
+++ src/Partners/Partners.js
@@ -35,7 +35,7 @@ const Partners = () => {
     return (
         <div class="relative bg-NAFPink pt-16 pb-24 overflow-hidden">
             <img src={Purple8Star1} class="z-10 absolute w-[350px] top-[0%]"></img>
-            <img src={Orange8Star1} class="z-10 absolute w-[310px] -top-[3.5%] right-[2%]"></img>
+            <img src={Orange8Star1} class="z-10 hidden lg:block absolute w-[310px] -top-[3.5%] right-[2%]"></img>
             <img src={BigWhiteDot} class="z-10 hidden lg:block absolute w-[32px] top-[12%] right-[13.5%]"></img>
             <img src={Yellow4Star1} class="z-10 absolute w-[135px] top-[9.8%] right-[3.5%]"></img>
             <img src={Blue4Star1} class="z-10 hidden lg:block absolute w-[200px] top-[23.6%] left-[0%]"></img>
@@ -49,29 +49,29 @@ const Partners = () => {
             <img src={Blue4Star3} class="z-10 hidden lg:block w-[170px] absolute bottom-[2%] right-[0%]"></img>
             <img src={Purple4Star1} class="z-10 hidden lg:block absolute w-[180px] -bottom-[4%] left-[16%]"></img>
             <img src={Yellow4Star2} class="z-10 hidden lg:block absolute w-[140px] -bottom-[2.5%] left-[46%]"></img>
-            <img src={Orange8Star2} class="z-10 absolute w-[270px] -bottom-[6%] right-[15%]"></img>
+            <img src={Orange8Star2} class="z-10 hidden lg:block absolute w-[270px] -bottom-[6%] right-[15%]"></img>
 
             <div class="relative z-20">
-                <img src={TextLogo} class="mx-auto w-[65%]"></img>
-                <p class="font-syne text-white text-center text-2xl pt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <img src={TextLogo} class="mx-auto w-[90%] lg:w-[65%]"></img>
+                <p class="font-syne text-white text-center text-lg md:text-2xl pt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div class="z-20 relative flex flex-wrap w-[90%] mx-auto text-center justify-between mt-20">
                 <div class="basis-[100%] min-[900px]:basis-[35%] min-[1600px]:basis-[27%] pt-8 pb-12 mb-12 bg-white border-4 border-black rounded-2xl shadow-[5px_5px_0_0_rgba(0,0,0)]">
                     <p class="text-3xl lg:text-4xl font-syneExtraBold mb-4">Organised by</p>
-                    <img src={NTUCACLogo} class="mx-auto h-[250px]"></img>
+                    <img src={NTUCACLogo} class="mx-auto h-[200px] lg:h-[250px]"></img>
                     <p class="text-xl font-syne">NTU Cultural Activities Club</p>
                 </div>
                 <div class="basis-[100%] min-[900px]:basis-[60%] min-[1600px]:basis-[40%] min-[1600px]:mx-6 pt-8 pb-12 mb-12 bg-white border-4 border-black rounded-2xl shadow-[5px_5px_0_0_rgba(0,0,0)]">
                     <p class="text-3xl lg:text-4xl font-syneExtraBold mb-4">Powered by</p>
                     <div class="flex flex-wrap px-4">
                         <div class="basis-[100%] md:basis-[50%]">
-                            <div class="flex h-[250px] justify-center">
-                                <img src={MyULifeLogo} class="mx-auto w-[222px]"></img>
+                            <div class="flex h-[200px] lg:h-[250px] justify-center">
+                                <img src={MyULifeLogo} class="mx-auto w-[180px] lg:w-[222px]"></img>
                             </div>
                             <p class="text-xl font-syne">MyULife</p>
                         </div>
                         <div class="basis-[100%] md:basis-[50%]">
-                            <div class="flex h-[250px] justify-center">
+                            <div class="flex h-[200px] lg:h-[250px] justify-center">
                                 
                             </div>
                             <p class="text-xl font-syne">Dorothy Cheung <br></br> Memorial Fund</p>
@@ -79,7 +79,7 @@ const Partners = () => {
                     </div>
                 </div>
                 <div class="basis-[100%] min-[1600px]:basis-[27%] pt-8 pb-12 mb-12 bg-white border-4 border-black rounded-2xl shadow-[5px_5px_0_0_rgba(0,0,0)]">
-                    <p class="text-3xl lg:text-4xl font-syneExtraBold mb-4 px-8">Official Productions Partner</p>
+                    <p class="text-2xl lg:text-4xl font-syneExtraBold mb-4">Official Productions Partner</p>
                 </div>
             </div>
 
