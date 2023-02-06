c40d2ef8fa7e7b23003c09451d06e05668262a71
anthonychew <anthony.chew@hotmail.com>
2023-02-07 02:16:44 +0800

    Fix profile page border not showing

--- src/Marketplace/Cart.js
+++ src/Marketplace/Cart.js
@@ -108,7 +108,7 @@ const Cart = () => {
             <div class="flex flex-col lg:flex-[25%] flex-[33%] items-center justify-center">
               <button onClick={postOrder}
                 type="button" class="mt-10 w-fit text-white border-4 border-black bg-#0071C6 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-large rounded-lg text-sm px-3 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                <p class="flex-1 md:text-buttonText_Desktop text-buttonText_Mobile font-syne text-center"> Check Out </p>
+                <p class="flex-1 lg:text-2xl text-sm font-syne text-center"> Check Out </p>
               </button>
             </div>
           </div>
